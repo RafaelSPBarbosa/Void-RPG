e4017669392b940578a615d92f402414799aa6c5
Rafael Barbosa <rafaspb11@gmail.com>
2024-05-11 10:51:27 -0300

    Added Abilities UI bar

---
+++ Godot Void RPG/Sources/Scripts/UI/Abilities/AbilitiesBar.gd
@@ -0,0 +1,22 @@
+class_name AbilitiesBar
+extends Node
+
+@export var Abilities: Array[AbilityBttn]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	for i in Abilities.size():
+		Abilities[i].cur_cooldown -= ( 1 * delta ) / Abilities[i].cooldown_duration
+		if(Abilities[i].cur_cooldown <= 0):
+			Abilities[i].cur_cooldown = 0
+		
+		if(Abilities[i].cur_cooldown <= 0):
+			if(Input.is_action_just_pressed(str("Ability0", (i + 1)))):
+				Abilities[i].cur_cooldown = 1.0
+		pass
+	pass
---
+++ Godot Void RPG/Sources/Scripts/UI/Abilities/AbilityBttn.gd
@@ -0,0 +1,16 @@
+class_name AbilityBttn
+
+extends TextureRect
+@onready var color_rect = $ColorRect
+@export var cur_cooldown:float = 1.0
+@export var cooldown_duration = 2
+@export var hidden_y_pos:float = 75
+
+func _ready():
+	#var tween = get_tree().create_tween() 
+	#tween.tween_property(color_rect, "position", Vector2(0,hidden_y_pos), 1).set_trans(Tween.TRANS_ELASTIC)
+	pass
+
+func _process(delta):
+	color_rect.position.y = lerp(0.0, hidden_y_pos, cur_cooldown)
+	pass
