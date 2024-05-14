65b4cf77020918f921ce084d296cefb66dd25ac5
Rafael Barbosa <rafaspb11@gmail.com>
2024-05-13 22:16:05 -0300

    Implemented dialog system

---
+++ Godot Void RPG/Sources/Scripts/Dialog/DialogSystem.gd
@@ -0,0 +1,149 @@
+class_name DialogSystem
+extends Node
+
+@onready var dim = $CanvasLayer/Control/Dim
+@onready var canvas_layer = $CanvasLayer
+
+@onready var tutorial_text = $CanvasLayer/Control/tutorial_text
+
+@onready var character_left = $CanvasLayer/Control/Character_Left
+@onready var dialog_left = $CanvasLayer/Control/Dialog_Left
+@onready var speaker_left = $CanvasLayer/Control/Dialog_Left/Speaker
+@onready var message_left = $CanvasLayer/Control/Dialog_Left/Message
+
+@onready var character_right = $CanvasLayer/Control/Character_Right
+@onready var dialog_right = $CanvasLayer/Control/Dialog_Right
+@onready var speaker_right = $CanvasLayer/Control/Dialog_Right/Speaker
+@onready var message_right = $CanvasLayer/Control/Dialog_Right/Message
+
+@export var placeholder_waddles: Texture
+@export var placeholder_capsule: Texture
+
+var is_visible = false
+var character_left_sprite: Texture = null
+var character_right_sprite: Texture = null
+
+enum Location {
+	Left,
+	Right
+}
+
+func _ready():
+	dim.self_modulate.a = 0.0
+	
+	character_left.set_position(Vector2(-1000, character_left.get_screen_position().y))
+	dialog_left.visible = false
+	
+	character_right.set_position(Vector2(1000, character_left.get_screen_position().y))
+	dialog_right.visible = false
+	
+	tutorial_text.visible = false
+	pass
+
+func _process(delta):
+	if(Input.is_action_just_pressed("move_backward")):
+		var dialog: Array
+		dialog = [
+			{
+			speaker_sprite = placeholder_waddles,
+			location = Location.Left,
+			speaker = "Waddles",
+			message = "Hey, I came as fast as I could!"
+			},
+			{
+			speaker_sprite = placeholder_capsule,
+			location = Location.Right,
+			speaker = "Capsule",
+			message = "Hey Waddles, good thing you're here! We need to clear out these void monkeys, can you defeat 10 of em for me?"
+			},
+			{
+			speaker_sprite = placeholder_waddles,
+			location = Location.Left,
+			speaker = "Waddles",
+			message = "Of course! Leave it to me, Mr. Capsule!"
+			}
+		]
+		
+		start_dialog(dialog)
+		pass
+	pass
+
+func start_dialog(dialog):
+	is_visible = true;
+	var tween = get_tree().create_tween() 
+	tween.tween_property(dim, "self_modulate:a", 1.0, 1).set_trans(Tween.TRANS_CUBIC)
+	
+	await get_tree().create_timer(1.0).timeout
+		
+	for line in dialog:
+		speak(line.speaker_sprite, line.location, line.speaker, line.message)
+		tutorial_text.visible = true
+		
+		await Engine.get_main_loop().process_frame
+		while(!Input.is_action_just_pressed("LMB")):
+			await Engine.get_main_loop().process_frame
+			pass
+		pass
+	
+	await Engine.get_main_loop().process_frame
+	while(!Input.is_action_just_pressed("LMB")):
+		await Engine.get_main_loop().process_frame
+	
+	tutorial_text.visible = false
+	
+	end_dialog()
+	pass
+	
+func end_dialog():
+	is_visible = false;
+	
+	dialog_left.visible = false
+	dialog_right.visible = false
+	
+	character_left_sprite = null
+	character_right_sprite = null
+	
+	var tween_left = get_tree().create_tween() 
+	tween_left.tween_property(character_left, "position", Vector2(-1000, character_left.get_screen_position().y) , 1).set_trans(Tween.TRANS_CUBIC)
+	
+	var tween_right = get_tree().create_tween() 
+	tween_right.tween_property(character_right, "position", Vector2(1000, character_right.get_screen_position().y) , 1).set_trans(Tween.TRANS_CUBIC)
+	await tween_left.finished
+	
+	var tween = get_tree().create_tween() 
+	tween.tween_property(dim, "self_modulate:a", 0.0, 1).set_trans(Tween.TRANS_CUBIC)
+	pass
+
+func speak(character_sprite: Texture, location: Location, name: String, message: String):
+	if(location == Location.Left):
+		dialog_right.visible = false
+		if(character_left_sprite == null || character_left_sprite != character_sprite):
+			character_left_sprite = character_sprite
+			character_left.texture = character_sprite
+			character_left.set_position(Vector2(-1000, character_left.get_screen_position().y))
+			var tween = get_tree().create_tween() 
+			tween.tween_property(character_left, "position", Vector2(-133, character_left.get_screen_position().y) , 1).set_trans(Tween.TRANS_CUBIC)
+			await tween.finished
+			pass
+		
+		speaker_left.text = name
+		message_left.text = message
+		dialog_left.visible = true
+		pass
+		
+	if(location == Location.Right):
+		dialog_left.visible = false
+		if(character_right_sprite == null || character_right_sprite != character_sprite):
+			character_right_sprite = character_sprite
+			character_right.texture = character_sprite
+			character_right.set_position(Vector2(1000, character_right.get_screen_position().y))
+			var tween = get_tree().create_tween() 
+			tween.tween_property(character_right, "position", Vector2(538, character_right.get_screen_position().y) , 1).set_trans(Tween.TRANS_CUBIC)
+			await tween.finished
+			pass
+		
+		speaker_right.text = name
+		message_right.text = message
+		dialog_right.visible = true
+		pass
+	pass
